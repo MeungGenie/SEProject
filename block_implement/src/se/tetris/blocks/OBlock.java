cebe6a8560568100b5b5b50182e397c90e172b16
KIM DOYEON <cat0806@naver.com>
2022-04-16 13:54:55 +0900

    블록 기본 구현

---
+++ block_implement/src/se/tetris/blocks/OBlock.java
@@ -0,0 +1,20 @@
+package se.tetris.blocks;
+
+import java.awt.Color;
+
+public class OBlock extends Block {
+
+	public OBlock() {
+		shape = new int[][] { 
+			{1, 1}, 
+			{1, 1}
+		};
+		color = Color.YELLOW;
+		colorBlind = new Color(187, 190, 242);
+		numOfBlockType = 1;
+	}
+	
+	public int[] getBlock() {
+	    return shape[r];
+	}
+}
---
+++ block_implement/src/se/tetris/blocks/TBlock.java
@@ -0,0 +1,20 @@
+package se.tetris.blocks;
+
+import java.awt.Color;
+
+public class TBlock extends Block {
+	
+	public TBlock() {
+		shape = new int[][] { 
+			{0, 1, 0},
+			{1, 1, 1}
+		};
+		color = Color.MAGENTA;
+		colorBlind = new Color(154, 127, 112);
+		numOfBlockType = 4;
+	}
+	
+	public int[] getBlock() {
+	    return shape[r];
+	}
+}
