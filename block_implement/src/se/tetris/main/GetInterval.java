d8f88a5e77bfcbb9ea8ff40629b9ab51a1514227
KIM DOYEON <cat0806@naver.com>
2022-05-25 17:11:41 +0900

    난이도에 따른 속도 증가 확인 unit test

---
+++ block_implement/src/se/tetris/main/GetInterval.java
@@ -0,0 +1,7 @@
+package se.tetris.main;
+
+public class GetInterval {
+	public double interval(double mode1, double mode2) { //mode1의 난이도가 mode2보다 쉬움
+		return mode1 - mode2;
+	}
+}
--- block_implement/src/se/tetris/main/GetIntervalTest.java
+++ block_implement/src/se/tetris/main/GetIntervalTest.java
@@ -1,5 +1,5 @@
 package se.tetris.main;
-import se.tetris.component.Board;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,13 +7,103 @@ import org.junit.jupiter.api.Test;
 class GetIntervalTest {
 
 	@Test
-	void testGetInterval() {
-		Board board = new Board();
-
-        assertEquals(board.getInterval(30, 0), 900);
-        assertEquals(board.getInterval(35, 3), 900);
-        assertEquals(board.getInterval(45, 5), 810);
-        assertEquals(board.getInterval(60, 10), 656);
+	void testSpeed() {
+		double easyRate = (1 - 0.1) + (0.1 * 0.2);
+		double normalRate = (1 - 0.1);
+		double hardRate = (1 - 0.1) - (0.1 * 0.2);
+		
+		GetInterval interval = new GetInterval();
+		for(int eraseCnt = 0; eraseCnt <31; eraseCnt++) {
+			if (eraseCnt < 5 && eraseCnt >= 0) {
+				if((eraseCnt + 1) % 5 == 0) {
+					System.out.println("지워진 줄 개수 : " + (eraseCnt + 1));
+					assertEquals(interval.interval(easyRate, normalRate), easyRate - normalRate);
+					assertEquals(interval.interval(normalRate, hardRate), normalRate - hardRate);
+					System.out.println("easy 속도 : " + String.format("%.6f",easyRate) +
+							"\tnormal 속도 : " + String.format("%.6f",normalRate)+ "\thard 속도 : " + String.format("%.6f",hardRate));
+					easyRate *= (1 - 0.1) + (0.1 * 0.2);
+					normalRate *= (1 - 0.1);
+					hardRate *= (1 - 0.1) - (0.1 * 0.2);
+				}
+	        }
+			else if (eraseCnt < 10 && eraseCnt >= 5) {
+				if((eraseCnt + 1) % 5 == 0) {
+					System.out.println("지워진 줄 개수 : " + (eraseCnt + 1));
+					assertEquals(interval.interval(easyRate, normalRate), easyRate - normalRate);
+					assertEquals(interval.interval(normalRate, hardRate), normalRate - hardRate);
+					System.out.println("easy 속도 : " + String.format("%.6f",easyRate) +
+							"\tnormal 속도 : " + String.format("%.6f",normalRate)+ "\thard 속도 : " + String.format("%.6f",hardRate));
+					easyRate *= (1 - 0.1) + (0.1 * 0.2);
+					normalRate *= (1 - 0.1);
+					hardRate *= (1 - 0.1) - (0.1 * 0.2);
+				}
+	        }
+			else if (eraseCnt < 15 && eraseCnt >= 10) {
+				if((eraseCnt + 1) % 5 == 0) {
+					System.out.println("지워진 줄 개수 : " + (eraseCnt + 1));
+					assertEquals(interval.interval(easyRate, normalRate), easyRate - normalRate);
+					assertEquals(interval.interval(normalRate, hardRate), normalRate - hardRate);
+					System.out.println("easy 속도 : " + String.format("%.6f",easyRate) +
+							"\tnormal 속도 : " + String.format("%.6f",normalRate)+ "\thard 속도 : " + String.format("%.6f",hardRate));
+					easyRate *= (1 - 0.1) + (0.1 * 0.2);
+					normalRate *= (1 - 0.1);
+					hardRate *= (1 - 0.1) - (0.1 * 0.2);
+				}
+	        }
+			else if (eraseCnt < 20 && eraseCnt >= 15) {
+				if((eraseCnt + 1) % 5 == 0) {
+					System.out.println("지워진 줄 개수 : " + (eraseCnt + 1));
+					assertEquals(interval.interval(easyRate, normalRate), easyRate - normalRate);
+					assertEquals(interval.interval(normalRate, hardRate), normalRate - hardRate);
+					System.out.println("easy 속도 : " + String.format("%.6f",easyRate) +
+							"\tnormal 속도 : " + String.format("%.6f",normalRate)+ "\thard 속도 : " + String.format("%.6f",hardRate));
+					easyRate *= (1 - 0.1) + (0.1 * 0.2);
+					normalRate *= (1 - 0.1);
+					hardRate *= (1 - 0.1) - (0.1 * 0.2);
+				}
+	        }
+			else if (eraseCnt < 25 && eraseCnt >= 20) {
+				if((eraseCnt + 1) % 5 == 0) {
+					System.out.println("지워진 줄 개수 : " + (eraseCnt + 1));
+					assertEquals(interval.interval(easyRate, normalRate), easyRate - normalRate);
+					assertEquals(interval.interval(normalRate, hardRate), normalRate - hardRate);
+					System.out.println("easy 속도 : " + String.format("%.6f",easyRate) +
+							"\tnormal 속도 : " + String.format("%.6f",normalRate)+ "\thard 속도 : " + String.format("%.6f",hardRate));
+					easyRate *= (1 - 0.1) + (0.1 * 0.2);
+					normalRate *= (1 - 0.1);
+					hardRate *= (1 - 0.1) - (0.1 * 0.2);
+				}
+	        }
+			else if (eraseCnt < 30 && eraseCnt >= 25) {
+				if((eraseCnt + 1) % 5 == 0) {
+					System.out.println("지워진 줄 개수 : " + (eraseCnt + 1));
+					assertEquals(interval.interval(easyRate, normalRate), easyRate - normalRate);
+					assertEquals(interval.interval(normalRate, hardRate), normalRate - hardRate);
+					System.out.println("easy 속도 : " + String.format("%.6f",easyRate) +
+							"\tnormal 속도 : " + String.format("%.6f",normalRate)+ "\thard 속도 : " + String.format("%.6f",hardRate));
+					easyRate *= (1 - 0.1) + (0.1 * 0.2);
+					normalRate *= (1 - 0.1);
+					hardRate *= (1 - 0.1) - (0.1 * 0.2);
+				}
+	        }
+			else if (eraseCnt >= 30) {
+				if((eraseCnt + 1) % 5 == 0) {
+					System.out.println("지워진 줄 개수 : " + (eraseCnt + 1));
+					assertEquals(interval.interval(easyRate, normalRate), easyRate - normalRate);
+					assertEquals(interval.interval(normalRate, hardRate), normalRate - hardRate);
+					System.out.println("easy 속도 : " + String.format("%.6f",easyRate) +
+							"\tnormal 속도 : " + String.format("%.6f",normalRate)+ "\thard 속도 : " + String.format("%.6f",hardRate));
+					easyRate *= (1 - 0.1) + (0.1 * 0.2);
+					normalRate *= (1 - 0.1);
+					hardRate *= (1 - 0.1) - (0.1 * 0.2);
+				}
+			}
+		}
+		
+		
+		
+		
+		
 	}
 
 }
