7210e6dd3504e6087274431b287055279ba14789
KIM DOYEON <cat0806@naver.com>
2022-05-27 09:07:44 +0900

    normal mode 유닛테스트 수정

--- block_implement/src/se/tetris/main/NormalTest.java
+++ block_implement/src/se/tetris/main/NormalTest.java
@@ -20,8 +20,6 @@ class NormalTest {
 		int normalZ = 0;
 		int normalS = 0;
 		
-		for(int j = 0; j < 10; j++)
-		{
 			for(int i =0; i < 1000; i++) {
 				block = (int)(Math.random() * 7);
 				switch(block) {
@@ -60,21 +58,20 @@ class NormalTest {
 			assertEquals(0, normal.percent((double)normalO / 1000, normalExpected), 5);
 			assertEquals(0, normal.percent((double)normalZ / 1000, normalExpected), 5);
 			
-			System.out.println("각 블럭이 등장해야 하는 확률 : " + String.format("%.6f",normalExpected) + " (전부 동일)");
-			System.out.print("I의 실제 등장 확률 : " + (double)normalI / 100);
-			System.out.println("\tI의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalI / 1000, normalExpected)));
-			System.out.print("J의 실제 등장 확률 : " + (double)normalJ / 100);
-			System.out.println("\tJ의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalJ / 1000, normalExpected)));
-			System.out.print("L의 실제 등장 확률 : " + (double)normalL / 100);
-			System.out.println("\tL의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalL / 1000, normalExpected)));
-			System.out.print("S의 실제 등장 확률 : " + (double)normalS / 100);
-			System.out.println("\tS의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalS / 1000, normalExpected)));
-			System.out.print("T의 실제 등장 확률 : " + (double)normalT / 100);
-			System.out.println("\tT의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalT / 1000, normalExpected)));
-			System.out.print("O의 실제 등장 확률 : " + (double)normalO / 100);
-			System.out.println("\tO의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalO / 1000, normalExpected)));
-			System.out.print("Z의 실제 등장 확률 : " + (double)normalZ / 100);
-			System.out.println("\tZ의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalZ / 1000, normalExpected)));
+			System.out.println("각 블럭이 등장해야 하는 확률 : " + String.format("%.6f",normalExpected * 100) + " (전부 동일)");
+			System.out.print("I의 실제 등장 확률 : " + (double)normalI / 10);
+			System.out.println("\tI의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalI / 1000, normalExpected* 10)));
+			System.out.print("J의 실제 등장 확률 : " + (double)normalJ / 10);
+			System.out.println("\tJ의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalJ / 1000, normalExpected* 10)));
+			System.out.print("L의 실제 등장 확률 : " + (double)normalL / 10);
+			System.out.println("\tL의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalL / 1000, normalExpected* 10)));
+			System.out.print("S의 실제 등장 확률 : " + (double)normalS / 10);
+			System.out.println("\tS의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalS / 1000, normalExpected* 10)));
+			System.out.print("T의 실제 등장 확률 : " + (double)normalT / 10);
+			System.out.println("\tT의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalT / 1000, normalExpected* 10)));
+			System.out.print("O의 실제 등장 확률 : " + (double)normalO / 10);
+			System.out.println("\tO의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalO / 1000, normalExpected* 10)));
+			System.out.print("Z의 실제 등장 확률 : " + (double)normalZ / 10);
+			System.out.println("\tZ의 오차 범위 : " + String.format("%.6f",normal.percent((double)normalZ / 1000, normalExpected* 10)));
 		}
-	}
 }
